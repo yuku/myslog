21addb569c90f2c26f1b6397ec15b7dcd19a4cfe
Yuku Takahashi <taka84u9@gmail.com>
2012-06-07 13:47:12 +0900

    add spec/myslog.rb

---
+++ spec/myslog.rb
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+require "spec_helper"
+
+describe "MySlog" do
+  before :each do
+    @myslog = MySlog.new
+  end
+
+  describe "#parse" do
+  end
+
+  describe "#parse_record" do
+  end
+end
