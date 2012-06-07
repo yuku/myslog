c36d3061b2c338868f8c38c57efd2c571bec26d8
Yuku Takahashi <taka84u9@gmail.com>
2012-06-07 13:47:57 +0900

    MySlog#parse_record should return an instance of Hash

--- spec/myslog.rb
+++ spec/myslog.rb
@@ -11,5 +11,22 @@ describe "MySlog" do
   end
 
   describe "#parse_record" do
+    before :all do
+      @record = <<-EOF
+# Time: 111003 14:17:38
+# User@Host: root[root] @ localhost []
+# Query_time: 0.000270  Lock_time: 0.000097 Rows_sent: 1  Rows_examined: 0
+SET timestamp=1317619058;
+SELECT * FROM life;
+      EOF
+    end
+
+    before :each do
+      @lines = @record.split("\n").map {|line| line.chomp}
+    end
+
+    it "should return an instance of Hash" do
+      @myslog.send(:parse_record, @lines).should be_an_instance_of Hash
+    end
   end
 end
